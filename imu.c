534f372463caef0f6483edde60e84edd9e80c7e2
John Wells <jfw@jfwhome.com>
2017-09-28 09:28:51 +0100

    header

--- imu.c
+++ imu.c
@@ -1,25 +1,24 @@
-////////////////////////////////////////////////////////////////////////////
-//
-//  This file is part of linux-mpu9150
-//
-//  Copyright (c) 2013 Pansenti, LLC
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy of 
-//  this software and associated documentation files (the "Software"), to deal in 
-//  the Software without restriction, including without limitation the rights to use, 
-//  copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
-//  Software, and to permit persons to whom the Software is furnished to do so, 
-//  subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in all 
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-//  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
-//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
-//  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/**
+ * 
+ * OpenVario MPU-9150 AHRS Driver
+ * (c) John Wells
+ * (c) Pansenti
+ * 
+ * Adapted from original code by Pansenti, Inc. See accompanying LICENSE file for details.
+ * 
+ * Reads the Invensense MPU9150 IMU and pushes fused euler angles to 
+ * TCP socket for XCSoar in a format compatiblewith the LevilAHRS driver..
+ * 
+ * Currently no attempt made to smooth output beyond what is done in the
+ * Invensense driver, or to transform the result for different Oenvario orientations.
+ * 
+ * TODO:
+ * Daemonise
+ * Add default orientation to calibration
+ * Update default sampling rate
+ * Integrate with OV sensord / sensorcal
+ * 
+ */
 
 #include <stdio.h>
 #include <stdint.h>
